8c6676e31287ccbb199cd4cba0485a12521cca89
DannyBoy13th <danielsolo13th@gmail.com>
2016-03-11 14:29:30 +0200

    Added files via upload

--- homework4/SpaceCounter/Main.java
+++ homework4/SpaceCounter/Main.java
@@ -1,4 +1,4 @@
-package com.goit.homework4.SpaceCounter;
+package homework4.SpaceCounter;
 
 import java.util.Scanner;
 
@@ -8,14 +8,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner ShapeScanner = new Scanner(System.in);
-        double a, b, c, h, r, result, result2, result3;
+        double h=14;
+        double c=7;
+        double a=5;
+        double b=8;
+        double r=6;
+        double pi = 3.1416;
+        double result;
+        double result2;
+        double result3;
 
 
         System.out.println("Space Counter");
         System.out.println();
         System.out.println("Please enter a shape which you would like to count (Triangle, Circle or Rectangle): ");
 
-        while (ShapeScanner.hasNext()) {
+        if (ShapeScanner.hasNext()) {
 
             if (ShapeScanner.hasNext("Triangle")) {
 
@@ -34,37 +42,41 @@ public class Main {
                 result = h * c / 2;
                 System.out.println("Result is: " + result);
 
+            }
 
-                if (ShapeScanner.hasNext("Circle")) {
 
-                    System.out.println("Please, print in the Radius of the Circle");
-                    if (ShapeScanner.hasNextDouble()) {
-                        r = ShapeScanner.nextDouble();
-                        System.out.println("Radius is: " + r);
+            if (ShapeScanner.hasNext("Circle")) {
+
+                System.out.println("Please, print in the Radius of the Circle");
+                if (ShapeScanner.hasNextDouble()) {
+                    r = ShapeScanner.nextDouble();
+                    System.out.println("Radius is: " + r);
                     }
 
-                    result2 = r * r / 3.14;
+                    result2 = r * r / pi;
                     System.out.println("Result is: " + result2);
                 }
-                if (ShapeScanner.hasNext("Rectangle")) {
+
+            if (ShapeScanner.hasNext("Rectangle")) {
                     System.out.println("Please print in Side A");
-                    if (ShapeScanner.hasNextDouble()) {
+
+                if (ShapeScanner.hasNextDouble()) {
                         a = ShapeScanner.nextDouble();
-                        System.out.println("Side A is: " + a);
+                    System.out.println("Side A is: " + a);
                     }
 
                     System.out.println("Please print in Side B");
-                    if (ShapeScanner.hasNextDouble()) {
+                if (ShapeScanner.hasNextDouble()) {
                         b = ShapeScanner.nextDouble();
-                        System.out.println("Side B is: " + b);
+                    System.out.println("Side B is: " + b);
                     }
 
-                    result3 = a * b;
-                    System.out.println("Result is: " + result3);
+                result3 = a * b;
+                System.out.println("Result is: " + result3);
 
                 }
             }
 
         }
     }
-}
+
